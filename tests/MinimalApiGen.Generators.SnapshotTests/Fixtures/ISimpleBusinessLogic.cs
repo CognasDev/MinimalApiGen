3e54aacb146bc4ccdef211de14719b6960ed7c64
CognasDev <hello@cognas.co.uk>
2025-01-04 06:49:12 +0000

    snapshot-testing > fixed bug for simple GET operation with no services

---
+++ tests/MinimalApiGen.Generators.SnapshotTests/Fixtures/ISimpleBusinessLogic.cs
@@ -0,0 +1,18 @@
+﻿namespace MinimalApiGen.Generators.SnapshotTests.Fixtures;
+
+/// <summary>
+/// 
+/// </summary>
+public interface ISimpleBusinessLogic
+{
+    #region Public Method Declarations
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<IEnumerable<SampleModel>> GetModelsAsync(CancellationToken cancellationToken);
+
+    #endregion
+}
