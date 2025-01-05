44a38d987ffe403e63dff1fc58eb7633ebdbbd6c
CognasDev <hello@cognas.co.uk>
2025-01-05 16:59:26 +0000

    multiple-statements > added integration tests

---
+++ tests/MinimalApiGen.Generators.IntegrationTests/Generators/GetMultipleLines.cs
@@ -0,0 +1,42 @@
+﻿using MinimalApiGen.Generators.IntegrationTests.Helpers;
+
+namespace MinimalApiGen.Generators.IntegrationTests.Generators;
+
+/// <summary>
+/// 
+/// </summary>
+public sealed class GetMultipleLines : IntegrationTestBase
+{
+    #region Property Declarations
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected override string Source => @"
+using MinimalApiGen.Framework.Generation;
+using MinimalApiGen.Generators.IntegrationTests.Fixtures;
+
+namespace TestNamespace;
+
+[QueryGenerator]
+public sealed class TestGenerator
+{
+    public TestGenerator()
+    {
+        ApiGeneration.Query<SampleModel>().WithNamespaceOf<ISimpleBusinessLogic>()
+                                          .WithGet()
+                                          .WithBusinessLogic<ISimpleBusinessLogic>(logic => logic.GetModelsAsync)
+                                          .WithResponse<SampleModelResponse>();
+
+        ApiGeneration.Query<SampleModel>().WithNamespaceOf<ISimpleBusinessLogic>()
+                                          .WithGet()
+                                          .WithBusinessLogic<ISimpleBusinessLogic>(logic => logic.GetModelsAsync)
+                                          .WithResponse<SampleModelResponse>()
+                                          .WithVersion(2);  
+
+        int dummyLine = 1;
+    }
+}";
+
+    #endregion
+}
---
+++ tests/MinimalApiGen.Generators.IntegrationTests/Snapshots/GetMultipleLines/GetMultipleLines.Output.02.verified.cs
@@ -0,0 +1,26 @@
+﻿//HintName: EndpointRouteMappingExtension.Query.g.cs
+using Microsoft.AspNetCore.Builder;
+using MinimalApiGen.Framework.Versioning;
+
+namespace MinimalApiGen.Framework.Generation;
+
+/// <summary>
+/// 
+/// </summary>
+public static class EndpointRouteMappingExtension
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="webApplication"></param>
+    public static void UseMinimalApiGenEndpointRouteMaps(this WebApplication webApplication)
+    {
+		MinimalApiGen.Generators.IntegrationTests.Fixtures.SampleModelQueryRouteEndpointsMapper mapperV1 = new();
+		RouteGroupBuilder apiVersionRouteV1 = webApplication.GetApiVersionRoute(1);
+		mapperV1.MapGetV1(apiVersionRouteV1);
+		MinimalApiGen.Generators.IntegrationTests.Fixtures.SampleModelQueryRouteEndpointsMapper mapperV2 = new();
+		RouteGroupBuilder apiVersionRouteV2 = webApplication.GetApiVersionRoute(2);
+		mapperV2.MapGetV2(apiVersionRouteV2);
+
+    }
+}
