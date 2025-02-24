ebc906f08bfe7fb36d90cf4f7190d031dc2235ce
CognasDev <hello@cognas.co.uk>
2025-02-24 07:36:58 +0000

    develop > blazor updates

---
+++ samples/Samples.MusicCollection.Web/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace Samples.MusicCollection.Web.Components.Layout;
+
+/// <summary>
+/// 
+/// </summary>
+public partial class MainLayout
+{
+    #region Property Declarations
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool SidebarExpanded { get; set; }
+
+    #endregion
+}
