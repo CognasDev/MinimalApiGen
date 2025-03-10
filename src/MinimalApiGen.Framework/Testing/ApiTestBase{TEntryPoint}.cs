c2595bc5f2f356f1e1cc6ea5320f9a9bb4b2754f
CognasDev <105775116+CognasDev@users.noreply.github.com>
2025-03-10 09:19:26 +0000

    api-testing > main (#58)
    * api-testing > added beginnings of API in memory testing code
    
    * api-testing > small update and nuget update

---
+++ src/MinimalApiGen.Framework/Testing/ApiTestBase{TEntryPoint}.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+using Xunit;
+
+namespace MinimalApiGen.Framework.Testing;
+
+/// <summary>
+/// 
+/// </summary>
+public abstract class ApiTestBase<TEntryPoint> : IAsyncLifetime, IClassFixture<TestServer<TEntryPoint>> where TEntryPoint : class
+{
+    #region Field Declarations
+
+    private readonly TestServer<TEntryPoint> _testServer;
+    private AsyncServiceScope _scope;
+
+    #endregion
+
+    #region Property Declarations
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static JsonSerializerOptions CaseInsensitiveSerializer { get; } = new() { PropertyNameCaseInsensitive = true };
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public IConfiguration Configuration { get; private set; } = default!;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public IServiceProvider ServiceProvider { get; private set; } = default!;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public HttpClient HttpClient { get; private set; } = default!;
+
+    #endregion
+
+    #region Constructor Declarations
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="testServer"></param>
+    protected ApiTestBase(TestServer<TEntryPoint> testServer) => _testServer = testServer;
+
+    #endregion
+
+    #region Public Method Declarations
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public async ValueTask InitializeAsync()
+    {
+        WebApplicationFactoryClientOptions options = new()
+        {
+            BaseAddress = new Uri("https://localhost/")
+        };
+        HttpClient = _testServer.CreateClient(options);
+        Configuration = _testServer.Configuration;
+        _scope = _testServer.Services.CreateAsyncScope();
+        ServiceProvider = _scope.ServiceProvider;
+        await Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public async ValueTask DisposeAsync()
+    {
+        HttpClient?.Dispose();
+        await _scope.DisposeAsync();
+    }
+
+    #endregion
+}
---
+++ src/MinimalApiGen.Framework/Testing/TestHttpClientFactory{TEntryPoint}.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MinimalApiGen.Framework.Testing;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="webApplicationFactory"></param>
+public sealed class TestHttpClientFactory<TEntryPoint>(WebApplicationFactory<TEntryPoint> webApplicationFactory) : IHttpClientFactory
+    where TEntryPoint : class
+{
+    #region Field Delarations
+
+    private readonly WebApplicationFactory<TEntryPoint> _webApplicationFactory = webApplicationFactory;
+
+    #endregion
+
+    #region Public Method Declarations
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public HttpClient CreateClient(string name) => _webApplicationFactory.CreateClient();
+
+    #endregion
+}
